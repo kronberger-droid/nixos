3cc9be9d8dfc2cef0c602878b0ae634d2e234984
Martin Kronberger <e12202316@student.tuwien.ac.at>
2024-12-08 16:35:15 +0100

    initialize zoxide for nushell

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,25 +1,39 @@
-# modules/shell.nix            
 { pkgs, ... }:
 {
-	programs.nushell = {
-		enable = true;
-		extraConfig = ''
-		  $env.config.show_banner = false
-		'';
-		shellAliases = {
-			cd = "zoxide";
-			cat = "bat";
-		};
-	};
+  programs.nushell = {
+    enable = true;
 
-	programs.starship = {
-		enable = true;
-		enableNushellIntegration = true;
-		settings = (with builtins; fromTOML (readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml")) // {
-		  time = {
-		    disabled = false;
-		    format = "[$time]($style) ";
-		  };
-		};
-	};
+    # Write the zoxide init output to a file
+    extraFiles."zoxide.nu" = {
+      text = builtins.readFile "${pkgs.zoxide}/share/zoxide/init.nu";
+    };
+
+    # Add zoxide sourcing and other configurations
+    extraConfig = ''
+      # Source zoxide.nu
+      source ~/.zoxide.nu
+
+      # Disable banner
+      $env.config.show_banner = false
+    '';
+
+    # Add shell aliases
+    shellAliases = {
+      cd = "zoxide";
+      cat = "bat";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+
+    # Add custom starship settings
+    settings = (with builtins; fromTOML (readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml")) // {
+      time = {
+        disabled = false;
+        format = "[$time]($style) ";
+      };
+    };
+  };
 }
