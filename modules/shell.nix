9bb5e2c210ca15396541eee1fd05e24448f60e04
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-01-04 14:36:22 +0100

    Update shell.nix

--- modules/shell.nix
+++ modules/shell.nix
@@ -27,6 +27,8 @@
 
     # Add custom starship settings
     settings = (with builtins; fromTOML (readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml")) // {
+      command_timeout = 2000;  # Global timeout (2 seconds for all commands)
+
       time = {
         disabled = false;
         format = "[$time]($style) ";
