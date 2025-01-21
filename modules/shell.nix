bdaaf2326fc42b7e7fcc8454c2576a8f658bad64
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-01-21 17:46:42 +0100

    change git branch symbol

--- modules/shell.nix
+++ modules/shell.nix
@@ -27,7 +27,7 @@
     # Add custom starship settings
     settings = (with builtins; fromTOML (readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml")) // {
       command_timeout = 2000;  # Global timeout (2 seconds for all commands)
-
+      git_branch.symbol = " ";
       time = {
         disabled = false;
         format = "[$time]($style) ";
