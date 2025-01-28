180b1956c7f5538c9e3079fee0e99e950397c3f9
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-01-28 21:39:24 +0100

    change to use way-displays

--- scripts/clamshell.sh
+++ scripts/clamshell.sh
@@ -6,7 +6,7 @@ LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
 read -r LS < "$LID_STATE_FILE"
 
 case "$LS" in
-*open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
-*closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
+*open)   way-displays -d DISABLED "$LAPTOP_OUTPUT";;
+*closed) way-displays -s DISABLED "$LAPTOP_OUTPUT";;
 *)       echo "Could not get lid state" >&2 ; exit 1 ;;
 esac
