7cc8617ba14118dafcfc2484ee8535b6d8ba58f5
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-02-02 13:07:00 +0100

    way-displays revert changes and remove minecraft launcher

--- modules/home-manager/kronberger.nix
+++ modules/home-manager/kronberger.nix
@@ -60,7 +60,6 @@
         rpi-imager
         keyd
         ipe
-        prismlauncher
         anydesk
       ];
       
--- modules/way-displays/way-displays.nix
+++ modules/way-displays/way-displays.nix
@@ -15,22 +15,28 @@
 		};
 	};
 
-  systemd.user.services.lid-switch-handler = {
-    description = "Run clamshell script when the lid state changes";
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      ExecStart = "${config.xdg.configHome}/way-displays/clamshell.sh";
-      Restart = "on-failure";
-    };
-  };
+ #  systemd.user.services = {
+	# 	lid-switch-handler = {
+	# 		Unit = {
+	# 			Description = "Run clamshell script when the lid state changes";
+	# 		};
+	#     Install = {
+	# 			WantedBy = [ "default.target" ];
+	# 		};
+	#     Service = {
+	#       ExecStart = "${config.xdg.configHome}/way-displays/clamshell.sh";
+	#       Restart = "on-failure";
+	#     };
+	#   };
+	# };
 	wayland.windowManager.sway.config.startup = lib.mkAfter [
 		{
 			command = "${pkgs.way-displays}/bin/way-displays > /tmp/way-displays.\${XDG_VTNR}.\${USER}.log 2>&1 &";
 			always = false;
 		}
-		# {
-		# 	command = "./clamshell.sh";
-		# 	always = true;		
-		# }
+		{
+			command = "${config.xdg.configHome}/way-displays/clamshell.sh";
+			always = true;		
+		}
 	];
 }
