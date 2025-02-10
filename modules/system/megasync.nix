d758f02de0b9b0c81dc323fc54e4de10e5ba7b8b
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-02-10 17:19:48 +0100

    adding megasync service

---
+++ modules/system/megasync.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    megasync
+  ];
+
+  systemd.user.services.megasync = {
+    description = "MegaSync Service (Wait for Waybar)";
+    after = [ "waybar.service" ];
+    wants = [ "waybar.service" ];
+    wantedBy = [ "default.target" ];  
+
+    serviceConfig = {
+      ExecStartPre = "${pkgs.bash}/bin/bash -c 'while ! ${pkgs.procps}/bin/pgrep -x .waybar-wrapped > /dev/null; do sleep 1; done'";
+      ExecStart = "${pkgs.megasync}/bin/megasync";
+      Restart = "on-failure";
+      RestartSec = "5s";  
+    };
+  };
+}
