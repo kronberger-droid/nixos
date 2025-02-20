e9aa0bb2b70a63e69f9ccbfcbbff9438ab53c37a
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-02-20 18:08:11 +0100

    add placeholders for t480s public keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
-	kronberger = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQY6sum7p25iggOW0SP/4iAeecD7PQy9IMIxQm6zRU+ e12202316@student.tuwien.ac.at";
-	intelNuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkg4KKoH9S5TtsMXmPkWxl6s4KgrdiPnqr2S9FW0fDv root@nixos-personal";
+	intelNucPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQY6sum7p25iggOW0SP/4iAeecD7PQy9IMIxQm6zRU+ e12202316@student.tuwien.ac.at";
+	intelNucSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkg4KKoH9S5TtsMXmPkWxl6s4KgrdiPnqr2S9FW0fDv root@nixos-personal";
+	t480sPersonal = "";
+	t480sSystem = "";
 in
 {
-	"cms-pswd.age".publicKeys = [ kronberger intelNuc];
+	"cms-pswd.age".publicKeys = [ intelNucPersonal intelNucSystem t480sPersonal t480sSystem];
 }
