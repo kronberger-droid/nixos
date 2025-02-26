fec7b6d56bc0631163a4d01b16a268d6d831db7d
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-02-27 00:54:25 +0100

    clamshell mode

--- modules/home-manager/way-displays.nix
+++ modules/home-manager/way-displays.nix
@@ -1,42 +1,22 @@
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 {
 	home = {
 		packages = with pkgs; [
 			way-displays
-			yq-go
 		];
 
-		file = {
-			".config/way-displays/clamshell.sh" = {
-				source = ./way-displays/clamshell.sh; 
-				executable = true;
-			};
-			".config/way-displays/cfg.yaml".source = ./way-displays/cfg.yaml;
-		};
+		file.".config/way-displays/cfg.yaml".source = ./way-displays/cfg.yaml;
 	};
-
- 	#  systemd.user.services = {
-	# 	lid-switch-handler = {
-	# 		Unit = {
-	# 			Description = "Run clamshell script when the lid state changes";
-	# 		};
-	#     Install = {
-	# 			WantedBy = [ "default.target" ];
-	# 		};
-	#     Service = {
-	#       ExecStart = "${config.xdg.configHome}/way-displays/clamshell.sh";
-	#       Restart = "on-failure";
-	#     };
-	#   };
-	# };
-	wayland.windowManager.sway.config.startup = lib.mkAfter [
-		{
-			command = "${pkgs.way-displays}/bin/way-displays > /tmp/way-displays.\${XDG_VTNR}.\${USER}.log 2>&1 &";
-			always = false;
-		}
-		{
-			command = "${config.xdg.configHome}/way-displays/clamshell.sh";
-			always = true;		
-		}
-	];
+	wayland.windowManager.sway = {
+		extraConfigEarly = lib.mkAfter ''
+			bindswitch --locked lid:on exec ${pkgs.way-displays}/bin/way-displays -s DISABLED "eDP-1"
+			bindswitch --locked lid:off exec ${pkgs.way-displays}/bin/way-displays -d DISABLED "eDP-1"
+		'';
+		config.startup = lib.mkAfter [
+			{
+				command = "${pkgs.way-displays}/bin/way-displays > /tmp/way-displays.\${XDG_VTNR}.\${USER}.log 2>&1 &";
+				always = false;
+			}
+		];
+	};
 }
