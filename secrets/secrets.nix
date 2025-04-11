1fd3e2246ca4835584ff968e80be800d0b4a48b6
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-04-11 18:51:13 +0200

    add t480s keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
 	intelNuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFijJelcEDGPlu9aDnjkLa4TWNXXJGeyHgw6ucANynAW intelNuc";
-	t480s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2TReHzHuuq6PEJB6z/NxnGHhpssDYu12BgA2Ku0+yb root@nixos";
+	t480s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWvSALLYRj7FIhpY+55a+dKl5s0Q3bxacot/xAqcnnQ t480s";
 in
 {
 	"cms-pswd.age".publicKeys = [ intelNuc t480s ];
