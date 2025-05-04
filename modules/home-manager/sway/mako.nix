5ffa2eb70eb527bda39340f24c73ecf3cc1be80c
kronberger-droid <kronberger@proton.me>
2025-05-04 16:34:14 +0200

    update

--- modules/home-manager/sway/mako.nix
+++ modules/home-manager/sway/mako.nix
@@ -7,10 +7,12 @@
   services = {
     mako = {
       enable = true;
-      defaultTimeout = 10000;
-      borderRadius = 8;
-      borderColor = accentColor;
-      backgroundColor = backgroundColor + "CC";
+      settings = {
+        defaultTimeout = "10000";
+        borderRadius = "8";
+        borderColor = accentColor;
+        backgroundColor = backgroundColor + "CC";
+      };
     };
     poweralertd.enable = true;
   };
