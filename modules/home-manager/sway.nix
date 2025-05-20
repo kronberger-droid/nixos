7e17db17a78d3fc7cfbdd52b7f82d4d276b8f4bc
kronberger-droid <kronberger@proton.me>
2025-05-20 10:55:07 +0200

    set keyring as SSH_AUTH

--- modules/home-manager/sway.nix
+++ modules/home-manager/sway.nix
@@ -84,6 +84,9 @@ in
     enable = true;
     wrapperFeatures.gtk = true;
     extraConfig = builtins.readFile "${./sway/config}";
+    extraSessionCommands = ''
+      export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/keyring/ssh
+    '';
     config = rec {
       modifier = modKey;
       terminal = "${pkgs.kitty}/bin/kitty";
