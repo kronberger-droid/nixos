f1e44692c72220db3f3e0f9433928593366a63ac
kronberger-droid <kronberger@proton.me>
2025-05-21 11:27:46 +0200

    rekey secret to be able to open on spectre

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-	intelNuc= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2nXGswPYhgVX6zwQAg3Wk8pfVw64pY+wIRIUoSyXYr root@intelNuc";
+	intelNuc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2nXGswPYhgVX6zwQAg3Wk8pfVw64pY+wIRIUoSyXYr root@intelNuc";
 	t480s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2TReHzHuuq6PEJB6z/NxnGHhpssDYu12BgA2Ku0+yb root@nixos";
 	spectre = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsAAo0+fQW+H+bvvsFnZ1FlvNdyeoZSnoUx4Y8V7Z/M root@spectre";
 in
