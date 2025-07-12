e5765b462987b850250118629baf0607ca3044d5
kronberger-droid <kronberger@proton.me>
2025-07-12 16:32:29 +0200

    fix keyring issue

--- modules/system/gnome-keyring.nix
+++ modules/system/gnome-keyring.nix
@@ -1,31 +1,25 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Enable GNOME Keyring for secrets and passwords
   services.gnome.gnome-keyring.enable = true;
   
-  # Enable PAM integration for automatic unlock on login
   security.pam.services = {
-    login.enableGnomeKeyring = true;
     swaylock.enableGnomeKeyring = true;
     greetd.enableGnomeKeyring = true;
   };
 
-  # Install required packages
   environment.systemPackages = with pkgs; [
     gnome-keyring
-    seahorse          # GUI for managing keyring
-    gcr_4                   # Modern SSH agent (gcr-ssh-agent)
-    libsecret               # For secret-tool CLI
+    seahorse
+    gcr_4
+    libsecret
   ];
 
-  # Enable gcr-ssh-agent socket (SSH_AUTH_SOCK will be set automatically)
   systemd.user.sockets.gcr-ssh-agent = {
     enable = true;
     wantedBy = [ "sockets.target" ];
   };
 
-  # The service will be started automatically by the socket
   systemd.user.services.gcr-ssh-agent = {
     enable = true;
   };
