12029eebf351e78380f60ce32b76075f12f4ee45
kronberger-droid <kronberger@proton.me>
2025-10-16 18:34:54 +0200

    update plus some minor improvements for emu

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -16,14 +16,12 @@
         package = pkgs.qemu_kvm;
         runAsRoot = false;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [ pkgs.OVMFFull.fd ];
-        };
       };
     };
   };
   programs.virt-manager.enable = true;
+
+  
   
   # Add your user to necessary groups
   users.users.kronberger = {
