439c4e198a39a41face30a6e76513bf1dab21ab1
kronberger-droid <kronberger@proton.me>
2025-10-19 18:43:27 +0200

    spotifyd and more cores for windows config

--- modules/home-manager/users/kronberger.nix
+++ modules/home-manager/users/kronberger.nix
@@ -37,6 +37,21 @@ in
       };
 
       services = {
+        spotifyd = {
+          enable = true;
+          settings = {
+            global = {
+              username = "martinkronberger";
+              use_keyring = true;
+              backend = "pulseaudio";
+              device_name = "intelNuc";
+              bitrate = 320;
+              volume_normalisation = true;
+              normalisation_pregain = -10;
+              device_type = "computer";
+            };
+          };
+        };
         gnome-keyring.enable = true;
       };
 
