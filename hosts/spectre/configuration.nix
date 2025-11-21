5c47f73b883f5f19c4d76520d29896779f3d7f96
kronberger-droid <kronberger@proton.me>
2025-11-21 12:31:09 +0100

    I give up on camera support on spectre

--- hosts/spectre/configuration.nix
+++ hosts/spectre/configuration.nix
@@ -43,7 +43,6 @@
     };
 
     udev.extraRules = ''
-      SUBSYSTEM=="video4linux", KERNEL=="video[0-9]*", ATTRS{name}=="Intel MIPI Camera", MODE="0660", GROUP="video"
       ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/leds/%k/brightness"
       ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/leds/%k/brightness"
     '';
@@ -51,11 +50,6 @@
 
   hardware = {
     enableRedistributableFirmware = true;
-    ipu6 = {
-      enable = true;
-      platform = "ipu6ep";
-    };
-    firmware = [pkgs.ipu6-camera-bins];
     keyboard.qmk.enable = true;
   };
 
@@ -100,16 +94,6 @@
   '';
 
   environment.systemPackages = with pkgs; [
-    v4l-utils
-    cheese
-    libcamera
-    libcamera-qcam
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.icamerasrc-ipu6ep
     brightnessctl
     light
     dmidecode
