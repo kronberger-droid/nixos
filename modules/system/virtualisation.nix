77d22a418ae4e1a497d5c9e3b2d4662045cd723d
kronberger-droid <kronberger@proton.me>
2025-12-01 19:31:00 +0100

    some qemu fixes

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
-{
-  # Install QuickEMU
+{pkgs, ...}: {
+  # Install QuickEMU and related packages
   environment.systemPackages = with pkgs; [
     quickemu
     qemu
     samba
     apptainer
+    spice
+    virt-viewer
+    mesa
+    virglrenderer
   ];
 
   # Enable KVM for better performance
@@ -17,12 +20,22 @@
         package = pkgs.qemu_kvm;
         runAsRoot = false;
         swtpm.enable = true;
+        verbatimConfig = ''
+          # Enable virgl renderer
+          nographics_allow_host_audio = 1
+        '';
       };
     };
   };
+
   programs.virt-manager.enable = true;
 
+  # Ensure OpenGL/DRI access
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; # If you need 32-bit support
+  };
+
   # Add virtualisation groups to the user
-  # Note: Main user configuration is in hosts/common.nix
-  users.users.kronberger.extraGroups = [ "libvirtd" "kvm" "qemu-libvirtd" ];
+  users.users.kronberger.extraGroups = ["libvirtd" "kvm" "qemu-libvirtd" "render"];
 }
